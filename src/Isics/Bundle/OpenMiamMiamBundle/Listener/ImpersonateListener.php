26498bd778a14991a70ee8e8a0933992910848c0
Steve Robinet <steve.robinet@isics.fr>
2014-09-15 10:27:58 +0200

    Add the listener to catch the error 500 exception on an already impersonated user who wants to impersonate

--- src/Isics/Bundle/OpenMiamMiamBundle/Listener/ImpersonateListener.php
+++ src/Isics/Bundle/OpenMiamMiamBundle/Listener/ImpersonateListener.php
@@ -2,17 +2,40 @@
 
 namespace Isics\Bundle\OpenMiamMiamBundle\Listener;
 
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 
+/**
+ * Class ImpersonateListener
+ * @package Isics\Bundle\OpenMiamMiamBundle\Listener
+ */
 class ImpersonateListener
 {
+    /**
+     * @var \Symfony\Bundle\FrameworkBundle\Templating\EngineInterface
+     */
+    protected $engine;
+
+    /**
+     * @param EngineInterface $engine
+     */
+    public function __construct(EngineInterface $engine) {
+        $this->engine = $engine;
+    }
+
+    /**
+     * Catch the error 500 page about someone you want to impersonate but already impersonate
+     *
+     * @param GetResponseForExceptionEvent $event
+     */
     public function onKernelException(GetResponseForExceptionEvent $event) {
         $exception = $event->getException();
 
-        ldd($exception);
-
-        if($exception->getCode() == 500 && preg_match('/You are already switched to ".*" user./', $exception->getMessage())) {
-
+        if(preg_match('/You are already switched to ".*" user./', $exception->getMessage())) {
+            $response = new Response();
+            $response->setContent($this->engine->render('IsicsOpenMiamMiamBundle:Admin:impersonate.html.twig'));
+            $event->setResponse($response);
         }
     }
 }
